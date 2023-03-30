aad178940aed4e0cdcb76b5c0feb8782a31f57d7
vadosiuoso <93610261+vadosiuoso@users.noreply.github.com>
2023-03-30 23:43:48 +0200

    Update MyHashMap.java
    Correct HashMap with generics.

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,87 +1,73 @@
 package Module9;
 
-import java.util.LinkedList;
-import java.util.Objects;
-
-public class MyHashMap {
-    LinkedList<Entry>[] hashMap = new LinkedList[2];
-    int size = 0;
-
-    public MyHashMap() {
-
+public class MyHashMap<K,V> {
+    private Node head;
+    public MyHashMap(){
+        this.head = null;
     }
-
-    public void put(Key key, Value value) {
-        if (size >= hashMap.length) {
-            resize();
+    private class Node<K, V>{
+        private K key;
+        private V value;
+        private Node next;
+        public Node(K key, V value){
+            this.key = key;
+            this.value = value;
+            this.next = null;
         }
-        int ix = getIndex(key) % hashMap.length;
-
-        if (hashMap[ix] == null) {
-            hashMap[ix] = new LinkedList<>();
-            hashMap[ix].add(new Entry(key, value));
-            size++;
-            return;
-        } else {
-            for (Entry entry : hashMap[ix]) {
-                if (entry.key.equals(key)) {
-                    entry.value = value;
-                    size++;
-                    return;
+    }
+    public void put(K key, V value) throws Exception {
+        Node current = this.head;
+        while (current != null){
+            if(current.key.equals(key)){
+                throw new Exception("Node with this key is already available");
+            }
+            current = current.next;
+        }
+        Node newNode = new Node<>(key, value);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+    public void remove(K key) throws Exception {
+        Node current = this.head;
+        Node previous = null;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                if (previous != null) {
+                    previous.next = current.next;
+                } else {
+                    this.head = current.next;
                 }
+                return;
             }
-            hashMap[ix].add(new Entry(key, value));
-            size++;
-            return;
+            previous = current;
+            current = current.next;
         }
+
+        throw new Exception("Node with this key not found");
     }
 
-    public Value get(Key key) {
-        int ix = getIndex(key) % hashMap.length;
-        if (hashMap[ix] == null) return null;
-
-        for (Entry entry : hashMap[ix]) {
-            if (entry.key.equals(key)) {
-                return entry.value;
-
-            }
-        }
-        return null;
+    public void clear(){
+        this.head = null;
     }
 
-    public void remove(Key key) {
-        if (key == null) return;
-        int ix = getIndex(key) % hashMap.length;
-        if (hashMap[ix] == null) return;
-        Entry toRemove = null;
-
-        for (Entry entry : hashMap[ix]) {
-            if (entry.key.equals(key)) {
-                toRemove = entry;
-
-            }
-        }
-        if (toRemove == null) return;
-        hashMap[ix].remove(toRemove);
-    }
-
-    public void resize() {
-        LinkedList<Entry>[] oldHashMap = hashMap;
-        hashMap = new LinkedList[size * 2];
-        size = 0;
-
-        for (int i = 0; i < oldHashMap.length; i++) {
-            if (oldHashMap[i] == null) continue;
-            for (Entry entry : oldHashMap[i]) {
-                put(entry.key, entry.value);
-            }
-        }
-    }
-    public int getIndex(Key key){
-        return key.hashCode();
-    }
     public int size(){
+        int size = 0;
+        Node current = this.head;
+        while (current != null){
+            size++;
+            current = current.next;
+        }
         return size;
+    }
+    public Object get(K key) throws Exception {
+        Node current = this.head;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
+        throw new Exception("Node with this key not found");
     }
 }
 
