f95d5f07a523f581594be9817374daeb9947f924
vadosiuoso <93610261+vadosiuoso@users.noreply.github.com>
2023-03-31 15:32:23 +0200

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,45 +1,43 @@
 package Module9;
 
-public class MyArrayList<E> {
-    private int initialCapacity;
-    private E[] array;
+public class MyArrayList<V> {
+    private V[] array;
+    private int size;
 
-    public MyArrayList() {
-        this.initialCapacity = 16;
-        this.array = (E[]) new Object[initialCapacity];
+    public MyArrayList(){
+        size = 16;
+        array = (V[]) new Object[size];
     }
 
-
-    public boolean add(E e) {
+    public void add(V value){
+        if(array[array.length-1] != null){
+            resizeArray();
+        }
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == null) {
-                array[i] = e;
-                return true;
+            if(array[i] == null){
+                array[i] = value;
+                break;
             }
         }
-        return false;
     }
-
-    public E get(int index) {
-        return array[index];
-    }
-
-    public E remove(int index){
-        E tmp = array[index];
+    public void remove(int index){
+        V tmp = array[index];
         if(index<size()){
             array[index] = null;
             createNewArray(array);
         }
-        return tmp;
+    }
+    public V get(int index){
+        return array[index];
     }
 
-    private void createNewArray(E[] array) {
-        E[] mass = (E[]) new Object[array.length];
+    private void createNewArray(V[] array) {
+        V[] mass = (V[]) new Object[array.length];
         int j = 0;
-        for(int i = 0;i< array.length;i++){
+        for(int i = 0; i< array.length; i++){
             mass[j] = (array[i] != null) ? array[i] : array[++i];
             j++;
-            if (array[i] == null && i+1<initialCapacity && array[i+1] == null){
+            if (array[i] == null && array[i+1] == null){
                 break;
             }
         }
@@ -55,15 +53,18 @@ public class MyArrayList<E> {
         }
         return count;
     }
+
     public void clear(){
-        for(int i = 0;i< array.length;i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = null;
         }
     }
-    public void peek(){
-
-    }
-    public void poll(){
-
+    public void resizeArray(){
+        size+=16;
+        V[] tmp = (V[]) new Object[size];
+        for (int i = 0; i < array.length; i++) {
+            tmp[i] = array[i];
+        }
+        array = tmp;
     }
 }
